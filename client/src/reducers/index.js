20b3a4702dfdfe6e541b058254ce4920a07c6c02
rushina-hash <115360895+rushina-hash@users.noreply.github.com>
2024-01-26 17:31:40 +0530

    reinitialized files

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+// import { combineReducers } from 'redux';
+
+// import posts from './posts';
+
+// 
+import { combineReducers } from 'redux';
+
+import posts from './posts';
+export default combineReducers({ posts });
+//export const reducers = combineReducers({ posts });
